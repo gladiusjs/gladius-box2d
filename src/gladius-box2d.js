67840eefb9c8696dcc69fd7f8dfb920dda9187a8
Alan Kligman <alan.kligman@gmail.com>
2012-05-16 17:13:13 -0400

    Initial commit.

---
+++ src/gladius-box2d.js
@@ -0,0 +1,33 @@
+if ( typeof define !== "function" ) {
+  var define = require( "amdefine" )( module );
+}
+
+define( function ( require ) {
+
+  var Extension = require( "base/extension" );
+
+  return new Extension( "gladius-box2d", {
+      
+      services: {
+        "resolver": {
+          service: require( "src/services/resolver" ),
+          components: {
+            Body: require( "src/components/body" )
+          },
+          resources: {
+          }
+        }
+      },
+      
+      components: {
+      },
+      
+      resources: {
+        BodyDefinition: require( "src/resources/body-definition" ),
+        FixtureDefinition: require( "src/resources/fixture-definition" ),
+        BoxShape: require( "src/resources/box-shape" )
+      }
+      
+  });
+
+});
---
+++ tools/jake-tasks/build.js
@@ -0,0 +1,17 @@
+var jake = require( "jake" );
+
+module.exports = function() {
+  var cmds = [
+              "node tools/r.js -o tools/build.js",
+              "uglifyjs --output dist/gladius-box2d.min.js dist/gladius-box2d.js"
+              ];
+  var callback = function() {
+  };
+  var opts = {
+      stdout: true,
+      stderr: true,
+      breakOnError: false
+  };
+
+  jake.exec( cmds, callback, opts );
+};
