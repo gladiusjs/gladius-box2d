9894070744414314c6d0aafda537ea1f342f2468
David Perit <dperit@gmail.com>
2012-07-19 17:32:09 -0400

    Included newer version of core and math libraries Fixed bug in onUpdate in body.js

--- src/components/body.js
+++ src/components/body.js
@@ -57,8 +57,8 @@ define( function ( require ) {
 
     // TD: This will cause the transform to emit an event that we handle below. Blech!
     var transform = this.owner.findComponent( "Transform" );
-    transform.setPosition( math.Vector3( position2.get_x(), position2.get_y(), transform.position[2] ) );
-    transform.setRotation( math.Vector3( transform.rotation.x, transform.rotation.y, angle2 ) );
+    transform.position = [ position2.get_x(), position2.get_y(), transform.position.z ];
+    transform.rotation = [ transform.rotation.x, transform.rotation.y, angle2 ];
   }
 
   function onEntitySpaceChanged( event ) {
